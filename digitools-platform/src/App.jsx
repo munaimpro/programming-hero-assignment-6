f77d90f22fa69dfe97872bf3d5fa73b6dd9e3ba6
Munaim Khan <khanmail2599@gmail.com>
2026-03-29 22:37:53 +0600

    project setup

---
+++ digitools-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <h2 className='text-red-600 font-bold bg-black py-8 text-2xl text-center'>Hello</h2>
+  )
+}
+
+export default App
