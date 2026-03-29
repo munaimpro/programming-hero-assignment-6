436526c8ee8ba37625d4d421c672b32d31d2cac7
Munaim Khan <khanmail2599@gmail.com>
2026-03-29 23:02:30 +0600

    content update to test deploy

--- digitools-platform/src/App.jsx
+++ digitools-platform/src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
 
   return (
-    <h2 className='text-red-600 font-bold bg-black py-8 text-2xl text-center'>Hello</h2>
+    <h2 className='text-red-600 font-bold bg-black py-8 text-2xl text-center'>Hello world</h2>
   )
 }
 
-export default App
+export default App 
